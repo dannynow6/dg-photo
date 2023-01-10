d2787cb3b0b28d5826b680e4759ae938b81b9c7a
Dan Garro <dobetterwithdan@gmail.com>
2023-01-10 17:45:26 -0500

    first model

--- dg_photography/models.py
+++ dg_photography/models.py
@@ -25,12 +25,26 @@ class Photo(models.Model):
         ("MF", "Medium-Format"),
         ("AC", "APS-C"),
         ("FT", "Micro Four Thirds"),
-        ("OF", "Other Format"),
+        ("OF", "One-Inch"),
     ]
 
     type = models.CharField(max_length=2, choices=TYPES_CHOICES)
-    location = models.CharField(max_length=125, blank=True)
-    name = models.CharField(max_length=125)
+    location_city = models.CharField(max_length=125, blank=True)
+    location_country = models.CharField(max_length=125, blank=True)
+    title = models.CharField(max_length=125)
     camera_make = models.CharField(max_length=100)
     camera_model = models.CharField(max_length=100)
     format = models.CharField(max_length=2, choices=FORMAT_CHOICES)
+    description = models.CharField(max_length=250, blank=True)
+    year_taken = models.DateField(auto_now_add=True)
+    lens_make = models.CharField(max_length=125, blank=True)
+    lens_model = models.CharField(max_length=125, blank=True)
+    focal_length = models.CharField(max_length=50, blank=True)
+
+    def __str__(self) -> str:
+        """return a string representation of model"""
+        title = self.title.title()
+        geo = self.location_country.title()
+        id = self.id
+        camera = self.camera_make.title()
+        return f"ID: {id} | {title} | Shot with {camera} | country: {geo}"

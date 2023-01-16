3cd2718f114f654305fbeff3928e49dbd2c6c809
Dan Garro <dobetterwithdan@gmail.com>
2023-01-16 15:41:22 -0500

    blog article model created, view created, html template created

--- photo_blog/models.py
+++ photo_blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogArticle(models.Model):
+    """
+    A model of a blog article
+    """
+
+    title = models.CharField(max_length=250, unique=True)
+    author = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="photos/", blank=True)
+    date_published = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=300)
+    article = models.TextField()
+
+    def __str__(self) -> str:
+        """Return a string representation of BlogArticle"""
+        title = self.title.title()
+        author = self.author.title()
+        date = self.date_published
+        return f"{author} | {title} | {date}"
--- photo_blog/views.py
+++ photo_blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+
+
+def photo_blog(request):
+    """Basic landing page for Photo Blog Articles"""
+    return render(request, "photo_blog/photo_blog.html")

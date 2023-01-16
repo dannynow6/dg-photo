972c70017ee080c93caefb6c114ff367d340f87f
Dan Garro <dobetterwithdan@gmail.com>
2023-01-16 16:02:46 -0500

    new blog article form

--- photo_blog/urls.py
+++ photo_blog/urls.py
@@ -1,5 +1,6 @@
 """ Defines URLs for photo_blog """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,4 +8,6 @@ app_name = "photo_blog"
 urlpatterns = [
     # A landing page for photo_blog
     path("photo_blog/", views.photo_blog, name="photo_blog"),
-]
+    # A page for submitting a new blog article
+    path("new_blog_article/", views.new_blog_article, name="new_blog_article"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photo_blog/views.py
+++ photo_blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import BlogArticle
+from .forms import BlogArticleForm
 
 # Create your views here
 
@@ -6,3 +8,18 @@ from django.shortcuts import render
 def photo_blog(request):
     """Basic landing page for Photo Blog Articles"""
     return render(request, "photo_blog/photo_blog.html")
+
+
+def new_blog_article(request):
+    """New Blog Article Form"""
+    if request.method != "POST":
+        form = BlogArticleForm()
+    else:
+        form = BlogArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("photo_blog:photo_blog")
+    context = {
+        "form": form,
+    }
+    return render(request, "photo_blog/new_blog_article.html", context)

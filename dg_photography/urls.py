bc1d7aa23d59300035bb1ecb0dd5fd3313f95c7e
Dan Garro <dobetterwithdan@gmail.com>
2023-01-17 13:31:40 -0500

    individual photos and articles are now viewable with images

--- dg_photography/urls.py
+++ dg_photography/urls.py
@@ -1,6 +1,6 @@
 """ Defines URL patterns for dg_photography """
-from django.conf import settings 
-from django.conf.urls.static import static 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -8,10 +8,14 @@ app_name = "dg_photography"
 urlpatterns = [
     # Home Page
     path("", views.index, name="index"),
-    # New Photo Form 
-    path("new_photo/", views.new_photo, name="new_photo"), 
+    # New Photo Form
+    path("new_photo/", views.new_photo, name="new_photo"),
     # A landing page for Dan G's Photography Website
     # path("dg_photos/", views.dg_photos, name="dg_photos"),
-    # About page for dg photography 
-    path("about/", views.about, name="about"), 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    # About page for dg photography
+    path("about/", views.about, name="about"),
+    # page for photos uploaded to site
+    path("photos/", views.photos, name="photos"),
+    # page for viewing individual photo details
+    path("photos/<int:photo_id>/", views.photo, name="photo"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dg_photography/views.py
+++ dg_photography/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 # from django.contrib.auth.decorators import login_required
 from .models import Photo
-from .forms import PhotoForm 
+from .forms import PhotoForm
 
 # Main site page
 def index(request):
@@ -28,6 +28,20 @@ def new_photo(request):
 
 def about(request):
     """dg photography about page"""
-    photos = Photo.objects.order_by('year_taken')
-    context = {'photos': photos}
+    photos = Photo.objects.order_by("year_taken")
+    context = {"photos": photos}
     return render(request, "dg_photography/about.html", context)
+
+
+def photos(request):
+    """dg photography page displaying photos uploaded to site"""
+    photos = Photo.objects.order_by("year_taken")
+    context = {"photos": photos}
+    return render(request, "dg_photography/photos.html", context)
+
+
+def photo(request, photo_id):
+    """show a single photo and its details"""
+    photo = Photo.objects.get(id=photo_id)
+    context = {"photo": photo}
+    return render(request, "dg_photography/photo.html", context)
